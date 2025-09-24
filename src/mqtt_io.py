7bbdc674f9557093f84cdb51d49f643e3cf11e18
Infernust <delvirasanchez@student.universidadunie.com>
2025-09-24 18:39:17 +0200

    mqtt_io.py

--- src/mqtt_io.py
+++ src/mqtt_io.py
@@ -1,13 +1,1 @@
-import paho.mqtt.client as mqtt
 
-def create_client(config, client_id, on_message=None):
-    client = mqtt.Client(client_id=client_id, protocol=mqtt.MQTTv311)
-
-    if config["username"]:
-        client.username_pw_set(config["username"], config["password"])
-
-    if on_message:
-        client.on_message = on_message
-
-    client.connect(config["broker"], config["port"], 60)
-    return client
