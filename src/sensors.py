36ae442fcd5081e1504466e4aff82fdbaf99f67d
Infernust <delvirasanchez@student.universidadunie.com>
2025-09-24 18:38:45 +0200

    sensors.py

--- src/sensors.py
+++ src/sensors.py
@@ -1,17 +1,1 @@
-import random
 
-def read_temperature():
-    return round(random.uniform(20.0, 30.0), 2)
-
-def read_humidity():
-    return round(random.uniform(40.0, 60.0), 2)
-
-def read_light():
-    return round(random.uniform(300.0, 800.0), 2)
-
-def read_all():
-    return {
-        "temperature": read_temperature(),
-        "humidity": read_humidity(),
-        "light": read_light(),
-    }
